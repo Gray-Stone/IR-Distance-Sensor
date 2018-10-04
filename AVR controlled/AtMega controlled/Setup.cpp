8593b259636842afff451e0a46fed5b506aca394
Leo Gray <leograyc@gmail.com>
2018-10-04 04:13:54 -0400

    Initial commit

---
+++ AVR controlled/AtMega controlled/Setup.cpp
@@ -0,0 +1,19 @@
+/* 
+* Setup.cpp
+*
+* Created: 2018/10/4 3:21:12
+* Author: leogr
+*/
+
+
+#include "Setup.h"
+
+// default constructor
+Setup::Setup()
+{
+} //Setup
+
+// default destructor
+Setup::~Setup()
+{
+} //~Setup
---
+++ AVR controlled/AtMega controlled/Setup.h
@@ -0,0 +1,31 @@
+/* 
+* Setup.h
+*
+* Created: 2018/10/4 3:21:12
+* Author: leogr
+*/
+
+
+#ifndef __SETUP_H__
+#define __SETUP_H__
+
+
+class Setup
+{
+//variables
+public:
+protected:
+private:
+
+//functions
+public:
+	Setup();
+	~Setup();
+protected:
+private:
+	Setup( const Setup &c );
+	Setup& operator=( const Setup &c );
+
+}; //Setup
+
+#endif //__SETUP_H__
